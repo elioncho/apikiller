cc64c8489c004874fc1f893be3a1f41e6e428403
elioncho <elioncho@gmail.com>
2018-11-20 14:32:08 -0500

    Move requests handler to execution model

--- index.rb
+++ index.rb
@@ -10,36 +10,7 @@ require_relative 'models/request'
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 
 execution_id = ARGV[0]
+
 @execution = Execution.find(execution_id)
-@requests = JSON.parse(@execution.collection.data)
-puts @execution.inspect
-puts @requests
-
-def call
-  EventMachine.run do
-    multi = EventMachine::MultiRequest.new
-    1.upto(@execution.rps) do |i|
-      request = Request.new(@requests.sample['request'])
-      multi.add i.to_s.to_sym, EventMachine::HttpRequest.new(request.url)
-                                                        .send(request.method,
-                                                              head: request.headers,
-                                                              body: request.body)
-    end
-  
-    multi.callback do
-      puts multi.responses[:callback]
-      puts multi.responses[:errback]
-      EventMachine.stop
-    end
-  end
-end
-
-scheduler = Rufus::Scheduler.new
-
-seconds = 0
-scheduler.every '1s' do
-  call
-  seconds += 1
-  scheduler.shutdown if seconds == @execution.exec_time
-end
-scheduler.join
+puts @execution.to_s
+@execution.start
--- models/execution.rb
+++ models/execution.rb
@@ -1,3 +1,40 @@
 class Execution < ActiveRecord::Base
   belongs_to :collection
+
+  def start
+    requests  = JSON.parse(collection.data)
+    scheduler = Rufus::Scheduler.new
+    seconds   = 0
+    scheduler.every '1s' do
+      do_requests(requests)
+      seconds += 1
+      scheduler.shutdown if seconds == exec_time
+    end
+    scheduler.join
+  end
+
+  def to_s
+    "id: #{id}, requests_per_second: #{rps}, running_time: #{exec_time}"
+  end
+
+  private
+
+  # TODO: move this elsewhwere
+  def do_requests(requests)
+    EventMachine.run do
+      multi = EventMachine::MultiRequest.new
+      1.upto(rps) do |i|
+        request = Request.new(requests.sample['request'])
+        multi.add i.to_s.to_sym, EventMachine::HttpRequest.new(request.url)
+                                                          .send(request.method,
+                                                                head: request.headers,
+                                                                body: request.body)
+      end
+      multi.callback do
+        puts multi.responses[:callback]
+        puts multi.responses[:errback]
+        EventMachine.stop
+      end
+    end
+  end
 end
