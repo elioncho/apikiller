0fd74ca33aa0169f23c508251933a656c7905447
elioncho <elioncho@gmail.com>
2018-11-20 21:44:48 -0500

    Remove unnecesary code from index.rb

--- index.rb
+++ index.rb
@@ -13,27 +13,5 @@ ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 
 execution_id = ARGV[0]
 @execution = Execution.find(execution_id)
-@requests = JSON.parse(@execution.collection.data)
-puts @execution.inspect
-puts @requests
-
-def call
-  EventMachine.run do
-    multi = EventMachine::MultiRequest.new
-    1.upto(@execution.rps) do |i|
-      request = Request.new(@requests.sample['request'])
-      multi.add i.to_s.to_sym, EventMachine::HttpRequest.new(request.url)
-                                                        .send(request.method)
-    end
-
-    multi.callback do
-      puts multi.responses[:callback]
-      puts multi.responses[:errback]
-      EventMachine.stop
-    end
-  end
-end
-
-@execution = Execution.find(execution_id)
 puts @execution.to_s
 @execution.start
